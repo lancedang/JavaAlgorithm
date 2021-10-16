ae1740acfcf304c1d59e5a6b2866b02a3fe54ed5
qiankai1 <qiankai1@xiaomi.com>
2021-10-16 21:32:09 +0800

    add array two number sum and inverse int number

--- src/main/java/dang/algorithm/leecode/InverseNumber.java
+++ src/main/java/dang/algorithm/leecode/InverseNumber.java
@@ -1,2 +1,36 @@
-package dang.algorithm.leecode;public class InverseNumber {
+package dang.algorithm.leecode;
+
+public class InverseNumber {
+    public static void main(String[] args) {
+
+        System.out.println(reverse(-2147483648));
+    }
+
+    public static int reverse(int x) {
+        int highLeft = x;
+        int result = 0;
+        if (highLeft == 0) {
+            return 0;
+        }
+        while (highLeft != 0) {
+            //1)获取余数，-123 % 10 = -3，目的：获取最后一位数不断*10
+            int lastNumber = highLeft % 10;
+
+            //2)max与min分别为 [−2^31, 2^31 − 1]， 即：[-2147483648, 2147483647]
+            //>7表示超过max, <-8超过min最小负数
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastNumber > 7)) {
+                return 0;
+            }
+
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastNumber < -8)) {
+                return 0;
+            }
+
+            //3)inverse值等于原来最后位*10+余数
+            result = result * 10 + lastNumber;
+            //4)一次余数取得后，向前递推
+            highLeft = highLeft / 10;
+        }
+        return result;
+    }
 }
--- src/main/java/dang/algorithm/leecode/SumInArray.java
+++ src/main/java/dang/algorithm/leecode/SumInArray.java
@@ -1,2 +1,32 @@
-package dang.algorithm.leecode;public class SumInArray {
+package dang.algorithm.leecode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SumInArray {
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 3};
+        int[] r = m2(a, 6);
+        System.out.println(r[0]+","+r[1]);
+
+        int x = -123;
+        System.out.println(x % 10);
+        System.out.println(x / 10);
+
+    }
+
+    public static int[] m2(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            //1)保存 value:index的map,通过hashMap的containsKey替代遍历数组
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i, map.get(target-nums[i])};
+            }
+            //2)数组有重复元素的情况，也支持
+            map.put(nums[i], i);
+        }
+
+        return  new int[2];
+    }
 }
